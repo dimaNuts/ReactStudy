a4cf01784840cd611dc95ff904b5e6fc93634b9a
dimaNuts <nuts272@mail.ru>
2023-08-21 14:49:25 +0300

    Create functions
    *create function increment
    *create function decrement
    *add functions for onClick

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,15 +2,22 @@ import React, {useState} from 'react';
 
 
 function App() {
-  const [count, setCount] = useState(5);
+  const [likes, setLikes] = useState(5);
 
+  function increment() {
+    setLikes(likes + 1)
+  }
+
+  function decrement() {
+    setLikes(likes - 1)
+  }
   return (
     <div className="App">
       <h1>{likes}</h1>
-      <button onClick={() => likes += 1}>
+      <button onClick={increment}>
        Increment
       </button>
-      <button onClick={() => likes -= 1}>
+      <button onClick={decrement}>
        Decrement
       </button>
     </div>
